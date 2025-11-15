2340a821d5ba53ce71058c6ca85d5522d21a8694
Mayuri Futane <futanemayuri0@gmail.com>
2025-11-15 22:29:31 +0530

    Initial commit

---
+++ Source Notepad/Notepad/About.java
@@ -0,0 +1,19 @@
+package Notepad;
+
+import javax.swing.*;
+
+public class About extends JFrame{
+
+    About() {
+        setBounds(400, 100, 600, 500);
+        setVisible(true);
+
+    }
+
+    public static void main(String [] args){
+        new About();
+
+    }
+
+}
+
